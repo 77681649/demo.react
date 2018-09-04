c873c7eb0bf89a76b177ba34050bab2e579d156e
seanwang <Sean.Wang@u-technologies.com>
2018-09-04 19:33:23 +0800

    add

--- src/umi-2.0-beta/2.0/packages/af-webpack/src/send.js
+++ src/umi-2.0-beta/2.0/packages/af-webpack/src/send.js
@@ -4,6 +4,10 @@ export const DONE = 'DONE';
 export const STARTING = 'STARTING';
 export const RESTART = 'RESTART';
 
+/**
+ * 发送消息个父进程
+ * @param {Any} message 消息
+ */
 export default function send(message) {
   if (process.send) {
     debug(`send ${JSON.stringify(message)}`);
--- src/umi-2.0-beta/2.0/packages/umi/bin/umi.js
+++ src/umi-2.0-beta/2.0/packages/umi/bin/umi.js
@@ -3,6 +3,7 @@
 const resolveCwd = require('resolve-cwd');
 
 const localCLI = resolveCwd.silent('umi/bin/umi');
+
 if (localCLI && localCLI !== __filename) {
   const debug = require('debug')('umi');
   debug('Using local install of umi');
