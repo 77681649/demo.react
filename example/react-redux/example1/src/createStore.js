59bc32e390f35847045e8f8fe1bd22923fbfcfa6
seanwang <seanwang@u-technologies.com>
2018-10-11 17:04:24 +0800

    更新react-redux实例

--- example/react-redux/example1/src/createStore.js
+++ example/react-redux/example1/src/createStore.js
@@ -1,6 +1,8 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducers'
+import logger from 'redux-logger'
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 export default function create() {
-  return createStore(rootReducer)
+  return createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)))
 }
