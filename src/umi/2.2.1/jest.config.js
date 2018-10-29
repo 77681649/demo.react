77436aaeb17726f3abb5c19347d030cb76377e26
seanwang <seanwang@u-technologies.com>
2018-10-29 23:03:49 +0800

    add files

---
+++ src/umi/2.2.1/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  testURL: 'http://localhost/',
+  testMatch: ['**/?*.(spec|test|e2e).(j|t)s?(x)'],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/examples/',
+    '/lib/',
+    '/packages/umi/src/scripts/test.js',
+    '/packages/umi/src/test.js',
+    '/packages/umi-plugin-dva/src/fixtures',
+    '/packages/umi-utils/src/fixtures',
+  ],
+  collectCoverageFrom: ['packages/**/src/**/*.{ts,tsx,js,jsx}'],
+  coveragePathIgnorePatterns: ['/packages/umi-plugin-dva/src/fixtures'],
+};
---
+++ src/umi/current/packages/umi-plugin-polyfills/src/index.js
@@ -0,0 +1,15 @@
+import { join, relative } from 'path';
+
+export default function(api, options = []) {
+  const { paths } = api;
+
+  api.addEntryPolyfillImports(() => {
+    return ['ie9', 'ie10', 'ie11']
+      .filter(key => {
+        return options.includes(key);
+      })
+      .map(key => ({
+        source: relative(paths.absTmpDirPath, join(__dirname, `${key}.js`)),
+      }));
+  });
+}
